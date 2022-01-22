58ad84a031e01d2350b6dde762889e6221492c9c
corey omalley <omalley242@gmail.com>
2022-01-22 17:52:25 +0000

    mainn

---
+++ FourierTransform/Fourier.js
@@ -0,0 +1,21 @@
+function FFTButtonPress(){
+    document.getElementById('FFTInput').classList.toggle('show');
+}
+
+
+function DFTButtonPress(){
+    document.getElementById('DFTInput').classList.toggle('show');
+
+}
+
+
+function CFTButtonPress(){
+    document.getElementById('CFTInput').classList.toggle('show');
+
+}
+
+
+function SETButtonPress(){
+    document.getElementById('SETInput').classList.toggle('show');
+
+}
