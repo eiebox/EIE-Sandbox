8579fc7d04d818f36f4053d7da4f8cd3bd7e47b2
omalley242 <omalley242@gmail.com>
2022-02-14 03:31:29 +0000

    added radix notation to EEP0

--- EEP0Assembler/EEP0Assembler.js
+++ EEP0Assembler/EEP0Assembler.js
@@ -133,9 +133,9 @@ function OpCodeResolver(Line) {
         // convert binary number back to int 
         // convert int to hex
         // make it uppercase and add leading 0s 
-        return parseInt(Line.join(''), 2).toString(16).toUpperCase().padStart(4, '0');
+        return "ox" + parseInt(Line.join(''), 2).toString(16).toUpperCase().padStart(4, '0');
     }
-    return Line.join('');
+    return "0b" + Line.join('');
 }
 
 
